494085005b3026e94a13e7d4baa8757db7fdd8a6
MateuxLucax <mateuxlucax@gmail.com>
2024-02-20 22:25:44 +0000

    refac: add workflow

--- load-test/user-files/simulations/rinhabackend/RinhaBackendCrebitosSimulation.scala
+++ load-test/user-files/simulations/rinhabackend/RinhaBackendCrebitosSimulation.scala
@@ -261,18 +261,18 @@ class RinhaBackendCrebitosSimulation
       criterioClienteNaoEcontrado.inject(
         atOnceUsers(1)
       ).andThen(
-        // debitos.inject(
-        //   rampUsersPerSec(1).to(220).during(2.minutes),
-        //   constantUsersPerSec(220).during(2.minutes)
-        // ),
-        // creditos.inject(
-        //   rampUsersPerSec(1).to(110).during(2.minutes),
-        //  constantUsersPerSec(110).during(2.minutes)
-        // ),
-        // extratos.inject(
-        //   rampUsersPerSec(1).to(10).during(2.minutes),
-        //   constantUsersPerSec(10).during(2.minutes)
-        // )
+        debitos.inject(
+          rampUsersPerSec(1).to(220).during(2.minutes),
+          constantUsersPerSec(220).during(2.minutes)
+        ),
+        creditos.inject(
+          rampUsersPerSec(1).to(110).during(2.minutes),
+         constantUsersPerSec(110).during(2.minutes)
+        ),
+        extratos.inject(
+          rampUsersPerSec(1).to(10).during(2.minutes),
+          constantUsersPerSec(10).during(2.minutes)
+        )
       )
     )
   ).protocols(httpProtocol)
